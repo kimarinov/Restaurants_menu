9bc1ae49b6b33e65940b5c8f5f5583ec2f06d804
kimarinov <58824713+kimarinov@users.noreply.github.com>
2020-11-10 19:43:22 +0200

    o
    o

---
+++ resources/views/restaurants/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.admin')
+@section('content')
+
+<h2>Restaurants</h2>
+<table class="table table-dark">
+	<thead>
+		<tr>
+			<th scope="col" class="text-center">#</th>
+			<th scope="col" class="text-center">Hall</th>
+		</tr>
+	</thead>
+	<tbody>
+		@php
+		$num = 1;
+		@endphp
+		@foreach( $restaurants as $restaurant )
+		<tr>
+			<td class="text-center"><?= $num++ ?></td>
+			<td class="text-center"><a href="" >{{ $restaurant->restaurant_name }}</a> </td>
+		</tr>
+		@endforeach
+	</tbody>
+</table>
+
+@endsection
