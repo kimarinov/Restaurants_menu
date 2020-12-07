3e103290a083c715a0ead74f28218dd4cab17e8a
kimarinov <58824713+kimarinov@users.noreply.github.com>
2020-12-07 21:19:21 +0200

    si

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -1,20 +1,42 @@
-<ul class="nav nav-tabs">
+<ul>
 	<li class="nav-item">
 		<a class="nav-link" href="{{route('HomePage')}}">Home</a>
 	</li>
-	<li class="nav-item">
-		<a class="nav-link" href="{{route('restaurants.index')}}">Restourants</a>
-	</li>
-	<li class="nav-item">
-		<a class="nav-link" href="{{route('dishes.index')}}">Meals</a>
-	</li>
-	<li class="nav-item">
-		<a class="nav-link" href="">Drinks</a>
-	</li>
-	<li class="nav-item">
-		<a class="nav-link" href="">Users</a>
-	</li>
+	@if( Auth::check())
+		<li class="nav-item">
+			<a class="nav-link" href="{{route('restaurants.index')}}">Restourants</a>
+		</li>
+		@if( Auth::user()->role->role_name == 'admin')
+			<li class="nav-item">		
+				<a class="nav-link" href="{{route('dishes.index')}}">Meals</a>		
+			</li>
+			<li class="nav-item">		
+				<a class="nav-link" href="">Drinks</a>		
+			</li>
+			<li class="nav-item">		
+				<a class="nav-link" href="">Users</a>		
+			</li>
+		@endif
+	 		<li class="nav-item">		
+				<a class="nav-link" href="{{ route('logout') }}"
+	       			onclick="event.preventDefault();
+	                document.getElementById('logout-form').submit();">
+	        		Logout
+			    </a>	
+			    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+	        		@csrf
+	    		</form>	
+			</li>
+		@else
+			<li class="nav-item">		
+				<a class="nav-link" href="{{route('login')}}">Login</a>		
+			</li>
+			<li class="nav-item">		
+				<a class="nav-link" href="{{route('register')}}">Register</a>		
+			</li>
+	@endif
 	<li class="nav-item">
 		<button id="theme-toggle" class="fas fa-{{ $theme == 'dark' ? 'sun' : 'moon' }} nav-link" onclick="toogle()"><i>Light/Dark</i></button>
 	</li>
-</ul>
+
+</ul> 
